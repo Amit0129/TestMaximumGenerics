04f4d08372a4302145539197ad33db23d23beb47
Amit0129 <amitkumarnayak40@gmail.com>
2022-12-02 19:42:42 +0530

    Given Three Integers find the maximum Integers

--- TestMaximum/Program.cs
+++ TestMaximum/Program.cs
@@ -5,6 +5,9 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Initializing Test Maximum Program");
+
+            int result = FindMaximum.GetMaximumIntNumber(95, 150, 65);
+            Console.WriteLine($"The maxumum number between three integer is : {result}");
         }
     }
 }
